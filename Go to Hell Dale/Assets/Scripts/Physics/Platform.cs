9d317c81b4579fe1650f4ee8ff1fa371e6e1c4ec
Kevin <eckstein.kevin@outlook.com>
2018-10-31 14:10:13 -0500

    Moving Platforms
    -Moving Platforms, prefab to come

---
+++ Go to Hell Dale/Assets/Scripts/Physics/Platform.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    Controller2D Player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+            Player = other.gameObject.GetComponent<Controller2D>();
+    }
+
+    void OnTriggerStay2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        { 
+            if (Player.collisions.below)
+            {
+                Debug.Log("Collisions below");
+
+                if (Player.transform.parent == null)
+                    Player.transform.parent = transform;
+            }
+            else
+            {
+                if (Player.transform.parent != null)
+                    Player.transform.parent = null;
+            }
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Player.transform.parent != null)
+                Player.transform.parent = null;
+
+            Player = null;
+        }
+    }
+}
