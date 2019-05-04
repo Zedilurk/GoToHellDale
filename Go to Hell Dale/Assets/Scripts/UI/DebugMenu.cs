23f655db2a34d0f6319d9b818cb32d4961a8d37a
Zedilurk <benkurdziel@yahoo.com>
2019-05-03 22:16:49 -0500

    Debug Modes work now + Spawn Checkpoint Debug Option

--- Go to Hell Dale/Assets/Scripts/UI/DebugMenu.cs
+++ Go to Hell Dale/Assets/Scripts/UI/DebugMenu.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DebugMenu : MonoBehaviour
 {
     public bool DebugMenuOpen = false;
+    public GameObject CheckpointPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -36,10 +38,28 @@ public class DebugMenu : MonoBehaviour
     public void NoClip ()
     {
         GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().NoClip();
+        GameObject myEventSystem = GameObject.Find("EventSystem");
+        myEventSystem.GetComponent<EventSystem>().SetSelectedGameObject(null);
+        DebugMenuOpen = false;
+        SetMenuVisibility();
     }
 
     public void God()
     {
         GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().God();
+        GameObject myEventSystem = GameObject.Find("EventSystem");
+        myEventSystem.GetComponent<EventSystem>().SetSelectedGameObject(null);
+        DebugMenuOpen = false;
+        SetMenuVisibility();
+    }
+
+    public void SpawnCheckpoint ()
+    {
+        GameObject myEventSystem = GameObject.Find("EventSystem");
+        myEventSystem.GetComponent<EventSystem>().SetSelectedGameObject(null);
+        Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Instantiate(CheckpointPrefab, playerPos, Quaternion.identity);
+        DebugMenuOpen = false;
+        SetMenuVisibility();
     }
 }
