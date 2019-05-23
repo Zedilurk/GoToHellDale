d923abb448dc75725a6f713439cfcd3ef44d1188
Zedilurk <benkurdziel@yahoo.com>
2019-05-22 21:13:11 -0500

    Jump Stack Prototype

---
+++ Go to Hell Dale/Assets/Scripts/Enemies/Stacked/StackEnemyChild.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackEnemyChild : MonoBehaviour
+{
+    public Vector3 StartPosition;
+    public Vector3 JumpPosition;
+    public float JumpSpeed;
+    //public float GraceDistance = .1f;
+    private float step = 0;
+
+    private bool _BouncingUpward = false;
+    private bool _BouncingDownward = false;
+    Coroutine BounceCoroutine = null;
+
+    private void FixedUpdate()
+    {
+        if (_BouncingUpward && BounceCoroutine == null)
+        {
+            BounceCoroutine = StartCoroutine(SmoothMove(StartPosition, JumpPosition, JumpSpeed));
+        }
+        else if (_BouncingUpward && BounceCoroutine != null)
+        {
+            if (Vector3.Distance(transform.localPosition, JumpPosition) <= 0)
+            {
+                _BouncingUpward = false;
+                _BouncingDownward = true;
+                StopCoroutine(BounceCoroutine);
+                BounceCoroutine = null;
+            }
+        }
+
+        if (_BouncingDownward && BounceCoroutine == null)
+        {
+            BounceCoroutine = StartCoroutine(SmoothMove(JumpPosition, StartPosition, JumpSpeed));
+        }
+        else if (_BouncingDownward && BounceCoroutine != null)
+        {
+            if (Vector3.Distance(StartPosition, transform.localPosition) <= 0)
+            {
+                _BouncingUpward = false;
+                _BouncingDownward = false;
+                StopCoroutine(BounceCoroutine);
+                BounceCoroutine = null;
+            }
+        }
+
+
+        /*
+        if (step < 1 && _BouncingUpward)
+        {
+            step += Time.deltaTime * JumpSpeed;
+            transform.localPosition = Vector3.Slerp(StartPosition, JumpPosition, step);
+        }   
+        else if (step >= 1 && _BouncingUpward)
+        {
+            step = 0;
+            _BouncingUpward = false;
+            _BouncingDownward = true;
+        }
+
+        if (step < 1 && _BouncingDownward)
+        {
+            step += Time.deltaTime * JumpSpeed;
+            transform.localPosition = Vector3.Slerp(JumpPosition, StartPosition, step);
+        }
+        else if (step >= 1 && _BouncingDownward)
+        {
+            step = 0;
+            _BouncingUpward = false;
+            _BouncingDownward = false;
+        }
+    */
+    }
+
+    public void Bounce ()
+    {
+        //step = 0;
+        BounceCoroutine = null;
+        _BouncingUpward = true;
+    }
+
+    IEnumerator SmoothMove(Vector3 startpos, Vector3 endpos, float seconds)
+    {
+        float t = 0.0f;
+        while (t <= 1.0)
+        {
+            t += Time.deltaTime / seconds;
+            transform.localPosition = Vector3.Lerp(startpos, endpos, Mathf.SmoothStep(0.0f, 1.0f, t));
+            yield return null;
+        }
+    }
+}
---
+++ Go to Hell Dale/Assets/Scripts/Enemies/Stacked/StackEnemyParent.cs
@@ -0,0 +1,64 @@
+﻿using Luminosity.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackEnemyParent : MonoBehaviour
+{
+    public float BounceAmount = 5f;
+    public float GapSpacesFromBottom = 1;
+    public float GapSize = 2;
+    public float JumpSpeed = 2;
+
+    public float TimeBetweenBounces = 4f;
+
+    private bool _IsBouncing = false;
+    public bool BeginBouncingOnSpawn = true;
+
+    public List<StackEnemyChild> EnemiesWithin = new List<StackEnemyChild>();
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /// Add all of our sub items that will bounce at start
+        foreach (Transform child in transform)
+            if (child.gameObject.tag == "Enemy")
+                EnemiesWithin.Add(child.GetComponent<StackEnemyChild>());
+
+        if (BeginBouncingOnSpawn)
+        {
+            _IsBouncing = true;
+            StartCoroutine(BounceLoop());
+        }      
+    }
+
+
+    IEnumerator BounceLoop ()
+    {
+        while (_IsBouncing)
+        {
+            Bounce();
+            yield return new WaitForSeconds(TimeBetweenBounces);
+        }
+    }
+
+    public void Bounce()
+    {
+        int x = 0;
+        foreach (StackEnemyChild enemy in EnemiesWithin)
+        {
+            float _BounceDistance = BounceAmount;
+            if (x >= GapSpacesFromBottom)
+                _BounceDistance += GapSize;
+
+            enemy.JumpSpeed = JumpSpeed;
+            enemy.StartPosition = enemy.transform.localPosition;
+            enemy.JumpPosition = enemy.transform.localPosition + new Vector3(0, _BounceDistance, 0);
+            enemy.Bounce();
+
+            x++;
+        }
+    }
+}
